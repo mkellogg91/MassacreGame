9bdfcf673701183a97b2aa7fff62aef99ea0fd25
Michael Kellogg <mkellogg91@gmail.com>
2015-09-01 09:18:15 -0400

    initial  publish!

---
+++ WarriorClassTest.BL/WarriorClassTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MassacreGame.BL;
+
+
+namespace WarriorClassTest.BL
+{
+    [TestClass]
+    public class WarriorClassTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Warrior warrior1 = new Warrior();
+            Warrior warrior2 = new Warrior();
+            Warrior winner;
+
+            warrior1.health = 200;
+            warrior1.strength = 5;
+            warrior1.isAlive = true;
+
+            warrior2.health = 200;
+            warrior2.strength = 5;
+            warrior2.isAlive = true;
+
+           winner = Warrior.fight(warrior1, warrior2);
+
+        }
+    }
+}
